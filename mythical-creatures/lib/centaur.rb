9e236cac5f771139d3fd1b0edbea1b11772ceba0
Elom Amouzou <elom1990@yahoo.com>
2019-11-29 17:19:41 -0700

    "Redid Centaur. Centaur tests pass."

--- mythical-creatures/lib/centaur.rb
+++ mythical-creatures/lib/centaur.rb
@@ -1,1 +1,94 @@
+class Centaur #ben posted
+  attr_reader :name, :breed, :cranky, :stand, :shoot_sound,
+  :run_sound, :rested
+  attr_accessor :shots, :runs
 
+  def initialize(name, breed = "Palomino")
+    @name = name
+    @breed = breed
+    @cranky = false
+    @stand = true
+    @shots = 0
+    @runs = 0
+    @shoot_sound = "Twang!!!"
+    @run_sound = "Clop clop clop clop!!!"
+    @rested = false
+  end
+
+  def shoot
+    if standing? == false                 #if laying, can't shoot
+      return "NO!"
+    elsif (@shots + @runs) >= 3           #shoot step 2
+      return "NO!"
+    elsif (@shots + @runs) < 3            #shoot step 1
+      @shots += 1
+      return @shoot_sound
+    end
+  end
+
+  def run
+    if standing? == false                 #if laying, can't run
+      return "NO!"
+    elsif (@shots + @runs) >= 3           #run step 2
+      return "NO!"
+    elsif (@shots + @runs) < 3            #run step 1
+      @runs += 1
+      return @run_sound
+    end
+  end
+
+  def cranky?
+    if (@shots + @runs) >= 3              #gets cranky after 3 s+r's
+      @cranky = true
+    elsif @cranky
+    end
+  end
+
+  def standing?
+    @stand
+  end
+
+  def sleep
+    if standing? == true                 # if centaur is standing, no sleep
+      return "NO!"
+    elsif standing? == false             # if laying, sleeps and removes crankiness
+      @shots = 0
+      @runs = 0
+    end
+  end
+
+  def lay_down
+    @stand = false
+  end
+
+  def laying?
+    if @stand == false
+      return true
+    end
+  end
+
+  def stand_up
+    @stand = true
+  end
+
+  def rested?
+    @rested
+  end
+
+  def drink_potion
+    if rested? == true
+      return "Oh no, Baby! Now you sick."
+    elsif standing? == true
+      @rested = true
+      return "Drank it!"
+    elsif standing? == false
+      return "Nah, son! Get up first."
+    end
+  end
+
+  def not_rested
+    @rested = false
+  end
+
+
+end
--- mythical-creatures/test/centaur_test.rb
+++ mythical-creatures/test/centaur_test.rb
@@ -11,37 +11,36 @@ class CentaurTest < Minitest::Test
   end
 
   def test_it_has_a_horse_breed
-    skip
     centaur = Centaur.new("George","Palomino")
     assert_equal "Palomino", centaur.breed
   end
 
   def test_it_has_excellent_bow_skills
-    skip
+
     centaur = Centaur.new("George","Palomino")
     assert_equal "Twang!!!", centaur.shoot
   end
 
   def test_it_makes_a_horse_sound_when_it_runs
-    skip
+
     centaur = Centaur.new("George","Palomino")
     assert_equal "Clop clop clop clop!!!", centaur.run
   end
 
   def test_when_first_created_it_is_not_cranky
-    skip
+
     centaur = Centaur.new("George","Palomino")
     refute centaur.cranky?
   end
 
   def test_when_first_created_it_is_standing_up
-    skip
+
     centaur = Centaur.new("George","Palomino")
     assert centaur.standing?
   end
 
   def test_after_running_or_shooting_a_bow_three_times_it_gets_cranky
-    skip
+
     centaur = Centaur.new("George","Palomino")
     refute centaur.cranky?
     centaur.shoot
@@ -51,27 +50,26 @@ class CentaurTest < Minitest::Test
   end
 
   def test_when_cranky_it_will_not_shoot_a_bow
-    skip
+
     centaur = Centaur.new("George","Palomino")
     3.times { centaur.shoot }
     assert_equal "NO!", centaur.shoot
   end
 
   def test_when_cranky_it_will_not_run
-    skip
     centaur = Centaur.new("George","Palomino")
     3.times { centaur.shoot }
     assert_equal "NO!", centaur.run
   end
 
   def test_when_standing_it_will_not_sleep
-    skip
+
     centaur = Centaur.new("George","Palomino")
     assert_equal "NO!", centaur.sleep
   end
 
   def test_after_laying_down_it_is_not_standing
-    skip
+
     centaur = Centaur.new("George","Palomino")
     centaur.lay_down
     refute centaur.standing?
@@ -79,28 +77,28 @@ class CentaurTest < Minitest::Test
   end
 
   def test_it_can_sleep_when_laying_down
-    skip
+
     centaur = Centaur.new("George","Palomino")
     centaur.lay_down
     refute_equal "NO!", centaur.sleep
   end
 
   def test_when_laying_down_it_cannot_shoot_a_bow
-    skip
+
     centaur = Centaur.new("George","Palomino")
     centaur.lay_down
     assert_equal "NO!", centaur.shoot
   end
 
   def test_when_laying_down_it_cannot_run
-    skip
+
     centaur = Centaur.new("George","Palomino")
     centaur.lay_down
     assert_equal "NO!", centaur.run
   end
 
   def test_it_can_stand_up
-    skip
+
 
     centaur = Centaur.new("George","Palomino")
     centaur.lay_down
@@ -109,7 +107,7 @@ class CentaurTest < Minitest::Test
   end
 
   def test_after_sleeping_it_is_no_longer_cranky
-    skip
+
 
     centaur = Centaur.new("George","Palomino")
 
@@ -131,18 +129,51 @@ class CentaurTest < Minitest::Test
   end
 
   def test_becomes_rested_after_drinking_a_potion
-    skip
+
     # your code here
+    centaur = Centaur.new("Lala","Djibouti")
+
+    refute centaur.rested?
+    assert "Drank it!", centaur.drink_potion
+    assert centaur.rested?
+
   end
 
   def test_can_only_drink_potion_while_standing
-    skip
+
     # your code here
+    centaur = Centaur.new("Lala","Djibouti")
+
+    refute centaur.rested?
+    assert centaur.standing?
+
+    centaur.lay_down
+    assert centaur.laying?
+
+    assert "Nah, son! Get up first.", centaur.drink_potion
+
+    centaur.stand_up
+    assert centaur.standing?
+    assert "Drank it!", centaur.drink_potion
+
   end
 
   def test_gets_sick_if_drinks_potion_while_rested
-    skip
+
     # your code here
+    centaur = Centaur.new("Lala","Djibouti")
+
+    refute centaur.rested?
+    assert "Drank it!", centaur.drink_potion
+    assert centaur.rested?
+    assert "Oh no, Baby! Now you sick.", centaur.drink_potion
+
+
+    centaur.not_rested
+
+    refute centaur.rested?
+    assert "Drank it!", centaur.drink_potion
+
   end
 
 end
