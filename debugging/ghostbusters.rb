6e3e46362468e395c310b2de6d1dd7d5a24d6885
Elom Amouzou <elom1990@yahoo.com>
2019-12-30 11:01:49 -0700

    Debugged ghostbusters

--- debugging/ghostbusters.rb
+++ debugging/ghostbusters.rb
@@ -1,21 +1,25 @@
 class Ghostbuster
+  attr_reader :name
 
   def initialize(name)
-    @nme = name
+    @name = name
   end
 
   def say_my_name
     puts "I'm #{name}"
   end
 
-  def get_car(car == "ECTO-1")
+  def get_car(car = "ECTO-1")
     @car = car
   end
 
+  def ride
+    get_car
+    puts "I'm riding the #{@car}."
+  end
 
 end
 
 ghostbuster = Ghostbuster.new("Ray")
 ghostbuster.say_my_name
 puts ghostbuster.ride
-
